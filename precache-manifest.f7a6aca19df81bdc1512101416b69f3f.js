297251c7a5d9cc9468c053e71abda1c30a30be10
china1006 <1414894071@qq.com>
2020-05-20 20:59:17 +0800

    deploy

---
+++ precache-manifest.f7a6aca19df81bdc1512101416b69f3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215d18255c930b510a9e2dd4266e599c",
+    "url": "/morney-2-react/index.html"
+  },
+  {
+    "revision": "a583a8284843058c9d1f",
+    "url": "/morney-2-react/static/css/main.9db7cb1a.chunk.css"
+  },
+  {
+    "revision": "0739ae12c7d53cbbc0e9",
+    "url": "/morney-2-react/static/js/2.7c1ee996.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/morney-2-react/static/js/2.7c1ee996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a583a8284843058c9d1f",
+    "url": "/morney-2-react/static/js/main.e43c3f72.chunk.js"
+  },
+  {
+    "revision": "f445be38e3043c9f224c",
+    "url": "/morney-2-react/static/js/runtime-main.899e2ef8.js"
+  }
+]);
